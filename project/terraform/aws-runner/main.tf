fb0c2a5e4eb6b06af05c83a8f75ba0663a5c0c36
RC-DevOps <r.cojan@yahoo.com>
2025-03-27 00:22:15 +0000

    Update main.tf

--- project/terraform/aws-runner/main.tf
+++ project/terraform/aws-runner/main.tf
@@ -2,7 +2,7 @@
 variable "aws_region" {
   description = "AWS region for the infrastructure"
   type        = string
-  default     = "us-east-1"  # Change this to your desired region
+  default     = "eu-central-1"  # Change this to your desired region
 }
 
 # Provider configuration
